825e2c8509fa871aa1b0a22769ebe22d5ee29c4e
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-11-02 21:47:06 -0500

    added websocket library to have stream feed

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,18 +1,48 @@
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Home: React.FC = () => {
     const [query, setQuery] = useState<string>("");
     const [info, setInfo] = useState<string>("");
+    const [imageSrc, setImageSrc] = useState<string>("");
 
     const handleDetect = () => {
-        setInfo(`Displays response`); // something happens here
+        setInfo(`${query}`); // something happens here
     };
+
+    useEffect(() => {
+        const ws = new WebSocket("ws://localhost:8000");
+
+        ws.onopen = () => {
+            console.log("WebSocket connection established");
+        };
+
+        ws.onmessage = (event) => {
+            const image = event.data;
+            setImageSrc(`data:image/jpeg;base64,${image}`);
+            }
+
+        ws.onclose = () => {
+            console.log("WebSocket connection closed");
+        };
+
+        return () => {
+            ws.close();
+        };
+    }, []);
 
     return (
         <div className="flex flex-col justify-start items-center h-screen bg-gradient-to-br from-blue-500 to-purple-600">
             <h1 className="text-center text-5xl font-extrabold mb-6 text-yellow-300 mt-10">SUS DETECTOR</h1>
-            <div className="flex mb-4 w-full max-w-xl mt-60">
+
+            <div className="w-full max-w-xl h-80 mb-2">
+                <img
+                    src={imageSrc}
+                    alt="Live Stream"
+                    className="w-full h-full object-cover rounded-lg"
+                />
+            </div>
+            <div className="flex mb-4 w-full max-w-xl">
                 <input
                     type="text"
                     placeholder="Enter Query"
